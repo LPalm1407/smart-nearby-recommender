117781b138f19868543c1413a7728367f51330a4
LPalm <gn2252@uni-rostock.de>
2026-02-19 10:47:30 +0100

    Add Mood buttons with state management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
 import "leaflet/dist/leaflet.css";
+import { useState } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import './App.css';
 
 
 function App() {
   const position = [51.505, -0.09];
+  const [mood, setMood] = useState("");
 
   return (
-    
-        <MapContainer center={position} zoom={13} scrollWheelZoom={true} style={{height: "100vh", width: "120vh"}}>
+    <div style={{margin: "10px"}}>
+      <button onClick={() => setMood("Work")}>Work</button>
+      <button onClick={() => setMood("Date")}>Date</button>
+      <button onClick={() => setMood("Quick Bite")}>Quick Bite</button>
+      <button onClick={() => setMood("Budget")}>Budget</button>
+
+      <p>Selected Mood: {mood || "None"}</p>
+
+      <MapContainer center={position} zoom={13} scrollWheelZoom={true} style={{height: "100vh", width: "120vh"}}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         </MapContainer>
+    </div>
+        
   )
 }
 
